b22bc16666cac03bce446ce4643defb4aeb40de7
HeysemLNU <ab224hv@student.lnu>
2019-11-03 15:17:18 +0100

    createdCloseWindow

--- src/js/app.js
+++ src/js/app.js
@@ -2,12 +2,28 @@ const appOpen = document.querySelector('#applicationOpen')
 const memoryButton = document.querySelector('#buttonMemory')
 const chatButton = document.querySelector('#buttonChat')
 const randomButton = document.querySelector('#buttonRandom')
+let memoGameWindAmount = 0
+// App Window
 const newWindow = document.createElement('div')
 newWindow.setAttribute('id', 'window')
+const closeButton = document.createElement('button')
+closeButton.setAttribute('type', 'button')
+closeButton.setAttribute('id', 'closeButtonWindow')
+closeButton.textContent = 'close'
+newWindow.appendChild(closeButton)
+
 // Functions
 const memoryButtonClicked = function () {
   memoryButton.addEventListener('click', () => {
-    appOpen.appendChild(newWindow)
+    const newMemoryWindow = newWindow
+    appOpen.appendChild(newMemoryWindow)
+    memoGameWindAmount++
+    const toRemove = memoGameWindAmount--
+    newMemoryWindow.firstChild.addEventListener('click', () => {
+      appOpen.removeChild(appOpen.childNodes[toRemove])
+      memoGameWindAmount--
+    })
+    console.log(memoGameWindAmount)
   })
 }
 memoryButtonClicked()
