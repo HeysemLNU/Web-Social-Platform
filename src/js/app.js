ef4a9882204f944b4a315f2aa4619b22d06e0f39
HeysemLNU <ab224hv@student.lnu>
2019-11-03 14:50:09 +0100

    createdButtons

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,13 @@
-console.log('Hello World!')
+const appOpen = document.querySelector('#applicationOpen')
+const memoryButton = document.querySelector('#buttonMemory')
+const chatButton = document.querySelector('#buttonChat')
+const randomButton = document.querySelector('#buttonRandom')
+const newWindow = document.createElement('div')
+newWindow.setAttribute('id', 'window')
+// Functions
+const memoryButtonClicked = function () {
+  memoryButton.addEventListener('click', () => {
+    appOpen.appendChild(newWindow)
+  })
+}
+memoryButtonClicked()
