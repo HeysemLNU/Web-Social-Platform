13b8f9b2511c57cda21121b49338796fea2054cc
heysem123 <38330164+heysem123@users.noreply.github.com>
2019-11-04 16:56:00 +0100

    trying to drag

--- src/js/app.js
+++ src/js/app.js
@@ -5,6 +5,7 @@ const randomButton = document.querySelector('#buttonRandom')
 let memoGameWindAmount = 0
 // App Window
 const newWindow = document.createElement('div')
+newWindow.setAttribute('draggable','true')
 newWindow.setAttribute('id', 'window')
 const closeButton = document.createElement('button')
 closeButton.setAttribute('type', 'button')
@@ -13,11 +14,18 @@ closeButton.textContent = 'close'
 newWindow.appendChild(closeButton)
 
 // Functions
+const dragFunct = function (toBeDraged){
+  toBeDraged.addEventListener('mousedown',(e) => {
+    
+  })
+}
 const memoryButtonClicked = function () {
   memoryButton.addEventListener('click', () => {
     const newMemoryWindow = newWindow
     appOpen.appendChild(newMemoryWindow)
     memoGameWindAmount++
+    const positionObj = newMemoryWindow.getBoundingClientRect()
+    console.log('your current position is: '+ positionObj.left)
     const toRemove = memoGameWindAmount--
     newMemoryWindow.firstChild.addEventListener('click', () => {
       appOpen.removeChild(appOpen.childNodes[toRemove])
